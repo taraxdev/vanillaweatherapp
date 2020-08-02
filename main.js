9f4475d33937a6cfc03f3ed00d57bc2b4cc0fd27
favpoint19 <56751101+favpoint19@users.noreply.github.com>
2020-08-02 13:54:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const api = {
-    key: "76435f8dfe6b4a52c859231b2ffd78ee
-    ",
-    baseurl: "https://api.openweathermap.org/data/2.5/",
+    key: "76435f8dfe6b4a52c859231b2ffd78ee",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
+    const searchbox = document.querySelector('.search-box');
+    searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt){
     if (evt.keyCode == 13) {
-        getResult(searchbox.value);
+        getResults(searchbox.value);
         console.log(searchbox.value);
     }
 }
 
-// function getResult (query) {
-//     fetch('${api.base}weather')
-// }
+function getResults (query) {
+    fetch('${api.base}weather?q=${query}&units=metric&APPID=${api.key}')
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+}
